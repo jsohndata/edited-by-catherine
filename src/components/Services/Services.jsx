38daef8d5b29256872a7b82e6559824e021f7dc9
jSohnData <jihos@me.com>
2023-05-25 11:32:47 -0400

    WIP updates

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -20,10 +20,10 @@ export default function Services() {
             <Accordion>
               <Accordion.Item eventKey="0">
                 <Accordion.Header>
-                  <Image src={`${imgUri}/images/site/services-author.webp`}
+                  {/* <Image src={`${imgUri}/images/site/services-author.webp`}
                       height="200"
                       alt=""
-                      className="me-3 d-none d-md-block" />                  
+                      className="me-3 d-none d-md-block" />                   */}
                   
                   <div className="service-content">
                     <h3>Authors</h3>
@@ -45,14 +45,16 @@ export default function Services() {
                   <h4>Query, proposal, and synopsis critiques and/or editing</h4>
                   <p>Available at an hourly rate of $150. Deliverables generally include a line-edited Word document, brief additional written notes as needed, and the option for follow-up questions via email or by phone. Please allow up to three hours for this service. Query editing may be included with multi-round developmental manuscript editing upon request and may also be added to other services. Payment is due upon completion of the work and receipt of an invoice.</p>
 
+                  <h4>Coaching and consulting services</h4>
+                  <p>For writers seeking either one-time general career/craft advice or on-going, dedicated personal support are available at an hourly rate of $195. A minimum of one hour is required. Flat fees or retainers also are available for on-going coaching work. All rates and fees will be mutually agreed upon in writing prior to any work and may be renegotiated as needed over the course of work.</p>
+
                   <h4>Short-term or smaller editorial projects</h4>
-                  <p>An hourly rate of $125 is available on a limited basis. A minimum of one hour is required, and an estimate will be provided prior to any work. Please note that this rate is reserved for single-round editing of short texts and other small projects only and is applied at Catherine Frank Editorial Services, LLC's discretion. An example of a smaller project might be a single round of editing for a picture book text. Deliverables may include written notes, margin notes in a Word document, and/or a follow-up phone call. Payment is due upon completion of the work and receipt of an invoice.</p>
+                  <p>An hourly rate of $125 is available on a limited basis. A minimum of one hour is required, and an estimate will be provided prior to any work. Please note that this rate is reserved for single-round editing of short texts and other small projects only and is applied at Catherine Frank Editorial Services, LLC's discretion. Deliverables may include written notes, margin notes in a Word document, and/or a follow-up phone call. Payment is due upon completion of the work and receipt of an invoice.</p>
 
                   <h4>Large scale or in-depth projects</h4>
                   <p>A flat fee will be charged. This might include single-round feedback for manuscripts longer than 4,000 words or deeper, multi-round developmental editing, which typically involves three rounds of deliverables such as a detailed editorial letter, manuscript line-editing in a Word document, and a final batch of notes and manuscript polishing. Follow-up phone calls are included with each round of work and communication between rounds of work via email is strongly encouraged. Single-round flat fees begin at $750 for picture book texts and $1,700 for longer fiction and nonfiction. Deeper, multi-round developmental editing (as described above) rates begin at $1,000 for picture book texts and $2,800 for longer fiction and nonfiction. All flat fee rates are subject to adjustment based on the size and scope of the project and will be mutually agreed upon in writing prior to any work. Payment terms will be set prior to any work. Payment is generally divided into segments and due at specified points during the course of work and upon completion.</p>
 
-                  <h4>Coaching and consulting services</h4>
-                  <p>For writers seeking either one-time general career/craft advice or on-going, dedicated personal support are available at an hourly rate of $195. A minimum of one hour is required. Flat fees or retainers also are available for on-going coaching work. All rates and fees will be mutually agreed upon in writing prior to any work and may be renegotiated as needed over the course of work.</p>
+                  
                   
                 
                   <h3 className="mt-5">Submission Policy for Authors:</h3>
@@ -70,10 +72,10 @@ export default function Services() {
             <Accordion>
               <Accordion.Item eventKey="1">
                 <Accordion.Header>
-                  <Image src={`${imgUri}/images/site/services-illustrator.webp`}
+                  {/* <Image src={`${imgUri}/images/site/services-illustrator.webp`}
                       height="200"
                       alt=""
-                      className="me-3 d-none d-md-block" />                 
+                      className="me-3 d-none d-md-block" />                  */}
                   
                   <div className="service-content">
                     <h3>Illustrators</h3>
@@ -105,10 +107,10 @@ export default function Services() {
             <Accordion>
               <Accordion.Item eventKey="2">
                 <Accordion.Header>
-                  <Image src={`${imgUri}/images/site/services-publisher.webp`}
+                  {/* <Image src={`${imgUri}/images/site/services-publisher.webp`}
                       height="200"
                       alt=""
-                      className="me-3 d-none d-md-block" />
+                      className="me-3 d-none d-md-block" /> */}
                   
                   <div className="service-content">
                     <h3>Publishers</h3>
@@ -122,13 +124,13 @@ export default function Services() {
 
                 <Accordion.Body>
                   <h4>Full-service Project Editing</h4>
-                  <p>I have extensive experience collaborating with in-house editorial teams, serving as lead editor for individual titles, and successfully meeting tight schedules. Please visit my <Link to="/#portfolio">Portfolio</Link> to review a selection of recent books I’ve edited.</p>
+                  <p>I have extensive experience collaborating with in-house editorial teams, serving as lead editor for individual titles, and successfully meeting tight schedules. Please visit my <Link to="/#portfolio">Portfolio</Link> to review a selection of recent books I've edited.</p>
 
                   <h4>Manuscript Consultation and Troubleshooting</h4>
-                  <p>I’m experienced in guiding challenging projects and orphaned projects through the complete production process, and in serving as a temporary or substitute editor on an as-needed basis.</p>
+                  <p>I'm experienced in guiding challenging projects and orphaned projects through the complete production process, and in serving as a temporary or substitute editor on an as-needed basis.</p>
 
                   <h4>Adaptation Services</h4>
-                  <p>I have significant experience adapting award-winning and best-selling adult books into successful young readers’ editions for major trade publishers. Please visit my <Link to="/#portfolio">Portfolio</Link> to review a selection of books I’ve adapted.</p>
+                  <p>I have significant experience adapting award-winning and best-selling adult books into successful young readers' editions for major trade publishers. Please visit my <Link to="/#portfolio">Portfolio</Link> to review a selection of books I've adapted.</p>
 
                   <p>Please <a href="mailto:catherine@editedbycatherine.com">email me</a> to talk about how I may help your editorial team.</p>
                 </Accordion.Body>
@@ -141,10 +143,10 @@ export default function Services() {
             <Accordion>
               <Accordion.Item eventKey="3">
                 <Accordion.Header>
-                <Image src={`${imgUri}/images/site/services-agent.webp`}
+                {/* <Image src={`${imgUri}/images/site/services-agent.webp`}
                       height="200"
                       alt=""
-                      className="me-3 d-none d-md-block" />
+                      className="me-3 d-none d-md-block" /> */}
 
                   <div className="service-content">
                     <h3>Agents</h3>
@@ -157,10 +159,10 @@ export default function Services() {
 
                 <Accordion.Body>
                   <h4>Manuscript Consultation</h4>
-                  <p>I’m available to provide feedback about manuscripts to agents and directly to authors, to serve as a sounding board or provide a second editorial opinion, and to offer other editorial support as needed.</p>
+                  <p>I'm available to provide feedback about manuscripts to agents and directly to authors, to serve as a sounding board or provide a second editorial opinion, and to offer other editorial support as needed.</p>
 
                   <h4>Manuscript Evaluation</h4>
-                  <p>I have extensive experience working with authors, illustrators, and agents to guide projects toward acquisition. I’m available to evaluate manuscripts and offer editorial feedback.</p>
+                  <p>I have extensive experience working with authors, illustrators, and agents to guide projects toward acquisition. I'm available to evaluate manuscripts and offer editorial feedback.</p>
 
                   <p>Please <a href="mailto:catherine@editedbycatherine.com"><em>email me</em></a> to talk about how I may help your team and your clients.</p>                  
                 </Accordion.Body>
