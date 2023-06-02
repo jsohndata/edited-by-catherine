fe996474143aaf33d61e494c8bc95fcb2f2ca1dd
jSohnData <jihos@me.com>
2023-06-02 13:35:04 -0400

    UPDATED text on purple bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,8 +17,9 @@ export default function Home() {
         <Intro />
         <Banner 
           textTitle={false}        
-          textLeft={<p><strong>Catherine Frank Editorial Services, LLC</strong>, can offer guidance, editorial feedback, and serve as a general liaison between you and the sometimes confusing world of children's publishing. I can help you turn the dream of <q>just wanting to be published</q> into a reality.</p>}          
-          textRight={<p>With twenty years of experience in children's publishing—more than a decade in the trenches at a Big Five trade imprint and almost as long as a freelance editor—I know the ins and outs of how to craft a query letter, how to find the right agent, and, most importantly, how to develop a manuscript and help a writer create the best possible version of a story.</p>} 
+          textLeft={<p><strong>Catherine Frank Editorial Services, LLC</strong>, can offer guidance, editorial feedback, and serve as a general liaison between you and the sometimes confusing world of children's publishing. I can help you turn the dream of <q>just wanting to be published</q> into a reality.</p>}
+
+          textRight={<p>With more than twenty years of experience in children's publishing—including over a decade in the trenches at a Big Five trade imprint—I know the ins and outs of how to craft a query letter, how to find the right agent, and, most importantly, how to develop a manuscript and help a writer create the best possible version of a story.</p>} 
         />
         <Gallery />
 
